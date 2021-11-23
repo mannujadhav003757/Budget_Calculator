0a69705754744e20333f5663074b93bf1df50137
manoj <mannujadhav003757@gmail.com>
2021-11-23 19:52:52 +0530

    validation issue fixes and working on Budget Component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,148 +1,190 @@
 import React from 'react'
-import { InputGroup, FormControl,Button,ListGroup } from 'react-bootstrap'
-import { useState } from 'react'
+import { InputGroup, FormControl, Button, ListGroup, Form } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
 
 export default function Budget() {
 
-const [budget,setBudget]=useState('')
-const [expense,setExpense]=useState('')
-const[amount,setAmount]=useState('')
-const [balance,setBalance]=useState()
+    const [budget, setBudget] = useState(0)
+    const [balance, setBalance] = useState(0)
+    const [totalExpense, setTotalExpense] = useState(0)
+    const [expense, setExpense] = useState('')
+    const [amount, setAmount] = useState('')
+    const [updateIndex, setUpdateIndex] = useState(0)
+    const [isUpdate, setIsupdate] = useState(true)
 
 
-const[items,setItems]=useState([])
-const[items1,setItems1]=useState([])
+
+    const expenseInput = React.useRef()
+    const amountInput = React.useRef()
+    const budgetInput = React.useRef()
 
 
-console.log(budget,expense,amount)
-var budget1=Number(budget)
-var amount1=Number(items)
-var totExpense=items.reduce((a, b) => a + b, 0)
-console.log("totexpense"+totExpense)
 
-function AddItem()
-{
-setItems1((oldItems1)=>{
-        return [...oldItems1,expense]
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        let tempExpense = 0;
+
+        items.forEach((el) => {
+            tempExpense += Number(el.value)
+        })
+        setTotalExpense(tempExpense)
+
+
+        let tempBalance = 0;
+
+        items.forEach((el) => {
+            tempBalance = tempBalance + Number(el.value)
+        })
+
+        setBalance(budget - tempBalance)
     })
-setItems((oldItems)=>{
-    return [...oldItems,Number(amount)]
-})
-}
-
-function calculate(bal)
-{
-    setBalance(budget1-totExpense)
-}
 
 
+    function AddItem(e) {
 
+        e.preventDefault()
 
+        setItems([
+            ...items,
+            {
+                title: expense,
+                value: Number(amount)
+            }
+        ]);
+        expenseInput.current.value = ''
+        amountInput.current.value = ''
+    }
 
+    function calculate() {
+        setBudget(budgetInput.current.value)
+        setBalance(budgetInput.current.value)
+        budgetInput.current.value = ''
+    }
+    
+    function deleteRecord(itemVal) {
+        let filteredArray = items.filter(item => item !== itemVal)
+        setItems(filteredArray);
+        setBalance(balance + Number(itemVal.value))
+
+    }
+    function updateRecord(itemVal, index) {
+        expenseInput.current.value = itemVal.title
+        amountInput.current.value = itemVal.value
+        setUpdateIndex(index)
+        setIsupdate(false)
+    }
+
+    function updateItem() {
+        items[updateIndex].title = expenseInput.current.value
+        items[updateIndex].value = amountInput.current.value
+        expenseInput.current.value = ''
+        amountInput.current.value = ''
+        setIsupdate(true)
+    }
     return (
         <>
-            <div className="container" style={{background:"lightgray"}}>
+            <div className="container" style={{ background: "lightgray" }}>
                 <h1> BUDGET APP</h1>
             </div>
-            <div className="container" style={{ border: "solid black", background:"lightgray" }}>
+            <div className="container" style={{ border: "solid black", background: "lightgray" }}>
                 <div className="row">
-                    <div className="col-sm-5" style={{ border: "solid green",marginTop:"10px",marginBottom:"10px",marginLeft:"10px"}}>
-                        <h4 style={{marginTop:"10px"}}>Please Enter Your Budget</h4>
+                    <div className="col-sm-5" style={{ border: "solid green", marginTop: "10px", marginBottom: "10px", marginLeft: "10px" }}>
+
+                        <h4 style={{ marginTop: "10px" }}>Please Enter Your Budget</h4>
+
                         <InputGroup className="mb-3">
 
                             <FormControl
                                 aria-label="Default"
+                                type="number"
                                 aria-describedby="inputGroup-sizing-default"
-                                onChange={(e)=>setBudget(e.target.value)}
+                                ref={budgetInput}
                             />
                         </InputGroup>
-                        <Button variant="light" style={{border:"solid  green" , marginBottom:"15px", marginTop:"10px"}} onClick={calculate}>Calculate</Button>
+                        <Button variant="light" style={{ border: "solid  green", marginBottom: "15px", marginTop: "10px" }} onClick={calculate}>Add Budget</Button>
 
                     </div>
-                    <div className="col-sm-2" style={{border:"solid thin blue"  ,marginTop:"10px",marginBottom:"10px", marginLeft:"20px"}}>
-                        <h4 style={{marginLeft:"25px"}}>BUDGET</h4>
-                        <img src="./img/money.png" style={{marginLeft:"35px"}} alt="money image"/>
-                        <h3 style={{marginLeft:"25px",marginTop:"10px"}}>{budget}</h3>
-                        
-                        
+                    <div className="col-sm-2" style={{ border: "solid thin blue", marginTop: "10px", marginBottom: "10px", marginLeft: "20px" }}>
+                        <h4 style={{ marginLeft: "25px" }}>BUDGET</h4>
+                        <img src="./img/money.png" style={{ marginLeft: "35px" }} alt="money image" />
+                        <h3 style={{ marginLeft: "25px", marginTop: "10px" }}>{budget}</h3>
+
+
 
                     </div>
-                    <div className="col-sm-2" style={{border:"solid thin blue"  ,marginTop:"10px",marginBottom:"10px", marginLeft:"20px"}}>
-                        <h4 style={{marginLeft:"25px"}}>EXPENSES</h4>
-                        <img src="./img/credit-card.png" style={{marginLeft:"35px"}} alt="money image"/>
-                        <h3 style={{marginLeft:"25px",marginTop:"10px"}}>{totExpense}</h3>
-                        
+                    <div className="col-sm-2" style={{ border: "solid thin blue", marginTop: "10px", marginBottom: "10px", marginLeft: "20px" }}>
+                        <h4 style={{ marginLeft: "25px" }}>EXPENSES</h4>
+                        <img src="./img/credit-card.png" style={{ marginLeft: "35px" }} alt="money image" />
+                        <h3 style={{ marginLeft: "25px", marginTop: "10px" }}>{totalExpense}</h3>
+
                     </div>
-                    <div className="col-sm-2" style={{border:"solid thin blue"  ,marginTop:"10px",marginBottom:"10px",marginLeft:"20px"}}>
-                        <h4 style={{marginLeft:"25px"}}>BALANCE</h4>
-                        <img src="./img/rupee-indian.png" style={{marginLeft:"35px"}} alt="money image"/>
-                        <h3 style={{marginLeft:"25px",marginTop:"10px"}}>{balance}</h3>
-                        
+                    <div className="col-sm-2" style={{ border: "solid thin blue", marginTop: "10px", marginBottom: "10px", marginLeft: "20px" }}>
+                        <h4 style={{ marginLeft: "25px" }}>BALANCE</h4>
+                        <img src="./img/rupee-indian.png" style={{ marginLeft: "35px" }} alt="money image" />
+                        <h3 style={{ marginLeft: "25px", marginTop: "10px" }}>{balance}</h3>
+
                     </div>
 
                 </div>
             </div>
-            <div className="container" style={{marginTop:"10px",border:"solid black",background:"lightgray"}}>
-                <div className="row" style={{marginTop:"10px"}}>
-                    
-                <div className="col-sm-4" style={{ border: "solid green",marginTop:"10px",marginBottom:"10px", marginLeft:"10px" }}>
-                        <h4 style={{marginTop:"10px"}}>Please Enter Your Expense</h4>
-                        <InputGroup className="mb-3">
+            <div className="container" style={{ marginTop: "10px", border: "solid black", background: "lightgray" }}>
+                <div className="row" style={{ marginTop: "10px" }}>
 
-                            <FormControl
-                                aria-label="Default"
-                                aria-describedby="inputGroup-sizing-default"
-                                onChange={(e)=>setExpense(e.target.value)}
-                            />
-                        </InputGroup>
-                        
+                    <div className="col-sm-4" style={{ border: "solid green", marginTop: "10px", marginBottom: "10px", marginLeft: "10px" }}>
+                        <h4 style={{ marginTop: "10px" }}>Please Enter Your Expense</h4>
+                        <Form>
+                            <InputGroup className="mb-3">
 
-                        <h4 style={{marginTop:"10px"}}>Please Enter Your Expense Amount</h4>
-                        <InputGroup className="mb-3">
+                                <FormControl
+                                    aria-label="Default"
+                                    aria-describedby="inputGroup-sizing-default"
+                                    name={expense}
+                                    onChange={(e) => setExpense(e.target.value)}
+                                    ref={expenseInput}
+                                />
+                            </InputGroup>
 
-                            <FormControl
-                                aria-label="Default"
-                                aria-describedby="inputGroup-sizing-default"
-                                onChange={(e)=>setAmount(e.target.value)}
-                            />
-                        </InputGroup>
-                        <Button variant="light" style={{border:"solid  green" , marginBottom:"15px", marginTop:"10px"}} onClick={AddItem}>Add Expense</Button>
 
+                            <h4 style={{ marginTop: "10px" }}>Please Enter Your Expense Amount</h4>
+                            <InputGroup className="mb-3">
+
+                                <FormControl
+                                    aria-label="Default"
+                                    type="number"
+                                    name={amount}
+                                    aria-describedby="inputGroup-sizing-default"
+                                    onChange={(e) => setAmount(e.target.value)}
+                                    ref={amountInput}
+                                />
+                            </InputGroup>
+                            {isUpdate ? <Button variant="light" style={{ border: "solid  green", marginBottom: "15px", marginTop: "10px" }} onClick={AddItem}>Add Expense</Button> :
+                                <Button variant="light" style={{ border: "solid  green", marginBottom: "15px", marginTop: "10px" }} onClick={updateItem} >Update Expense</Button>}
+                        </Form>
 
                     </div>
-                    <div className="col-sm-3" style={{border:"solid thin blue"  ,marginTop:"10px",marginBottom:"10px", marginLeft:"20px"}}>
-                        <h4 style={{marginLeft:"25px"}}>EXPENSE TITLE</h4>
+
+
+                    <div className="col-sm-7" style={{ border: "solid thin blue", marginTop: "10px", marginBottom: "10px", marginLeft: "20px" }}>
+                        <h4 style={{ marginLeft: "25px" }}>EXPENSE VALUE</h4>
                         <ListGroup>
                             {
-                            items1.map((itemval)=>{
-                                return <ListGroup.Item>{itemval}</ListGroup.Item>
-                            })
-                        }
-                        </ListGroup>
-                        
-                        
-                        
-                        
-                    </div>
-                    <div className="col-sm-4" style={{border:"solid thin blue"  ,marginTop:"10px",marginBottom:"10px", marginLeft:"20px"}}>
-                        <h4 style={{marginLeft:"25px"}}>EXPENSE VALUE</h4>
-                        <ListGroup>
-                            {
-                                items.map((itemVal)=>{
-                                    return <ListGroup.Item style={{color:"red"}}>Rs.{itemVal}/- <span style={{marginLeft:"180px"}}><img src="./img/writing.png" /><img src="./img/bin.png" style={{marginLeft:"20px"}}/></span></ListGroup.Item>
+                                items.map((itemVal, index) => {
+                                    return <ListGroup.Item style={{ color: "red" }}>{itemVal.title} - Rs.{itemVal.value}/- <span style={{ marginLeft: "180px" }}><img src="./img/writing.png" onClick={() => updateRecord(itemVal, index)} /><img src="./img/bin.png" style={{ marginLeft: "20px" }} onClick={() => deleteRecord(itemVal)} /></span></ListGroup.Item>
                                 })
                             }
                         </ListGroup>
-                       
-                        
-                        
-                        
+
+
+
+
+
                     </div>
-                    
-                    
+
+
 
                 </div>
-                </div>
+            </div>
         </>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,123 +1,135 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap'
-import { useState,useEffect } from 'react'
-import {useNavigate} from 'react-router-dom'
-import  axios  from 'axios'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 
 export default function Login() {
+    const responseFacebook = (response) => {
+        console.log("Log in result here:" + response)
+        navigate('/budget')
+    }
+    const componentClicked = (data) => {
+        console.log("here is data:" + data)
+    }
+    const responseGoogle = (response) => {
+        console.log("Here is Gooogle response data:"+response)
+        navigate('/budget')
+      }
     
-        const navigate=useNavigate()
-        //states for inputs
-        const[uname,setUname]=useState('')
-        const[email,setEmail]=useState('')
-        const[pwd,setpwd]=useState('')
-        const [state,setState]=useState([])
+    const navigate = useNavigate()
+    //states for inputs
+    const [uname, setUname] = useState('')
+    const [email, setEmail] = useState('')
+    const [pwd, setpwd] = useState('')
+    const [state, setState] = useState([])
 
-        //states for errror display
-        const[uerr,setUerr]=useState(false)
-        const[emailerr,setEmailerr]=useState(false)
+    //states for errror display
+    const [uerr, setUerr] = useState(false)
+    const [emailerr, setEmailerr] = useState(false)
 
-        // async function validate(e)
-        // {
-        //     e.preventDefault()
-        //     var item={uname,email,pwd}
-        //     var result=await fetch('http://localhost:4001/record',{
-        //         method:'GET',
-        // headers:{
-        //     'Accept':'application/json',
-        //     'content-type':'application/json'
-        //         },
-        // body:JSON.stringify(item)
-        //     });
-        //     result = await result.json();
-        //     localStorage.setItem('user-info',JSON.stringify(result))
-        //     console.log("here is the result of log in"+result)
-        //     alert("log in successfull");
+    // async function validate(e)
+    // {
+    //     e.preventDefault()
+    //     var item={uname,email,pwd}
+    //     var result=await fetch('http://localhost:4001/record',{
+    //         method:'GET',
+    // headers:{
+    //     'Accept':'application/json',
+    //     'content-type':'application/json'
+    //         },
+    // body:JSON.stringify(item)
+    //     });
+    //     result = await result.json();
+    //     localStorage.setItem('user-info',JSON.stringify(result))
+    //     console.log("here is the result of log in"+result)
+    //     alert("log in successfull");
 
-        // }
-        // useEffect(() => {
-        //     axios.get('http://localhost:4001/record').then(resp => {
-        //       //console.log(JSON.stringify(resp))
-        //       console.log("here is original response"+resp)
-        //       const temp=resp.data
-        //       console.log("Here is the COnsole"+resp.data)
-        //       setState([resp])
-        //       console.log("here is the exact resposnse"+state)
-        //     }).catch(err => {
-        //       console.log(err)
-        //     })
-        //   }, [])
-        useEffect(() => {
-            axios.get('http://localhost:4001/record').then(resp => {
-              console.log(resp)
-              setState(resp.data)
-              console.log(state)
-            }).catch(err => {
-              console.log(err)
-            })
-          }, [])
+    // }
+    // useEffect(() => {
+    //     axios.get('http://localhost:4001/record').then(resp => {
+    //       //console.log(JSON.stringify(resp))
+    //       console.log("here is original response"+resp)
+    //       const temp=resp.data
+    //       console.log("Here is the COnsole"+resp.data)
+    //       setState([resp])
+    //       console.log("here is the exact resposnse"+state)
+    //     }).catch(err => {
+    //       console.log(err)
+    //     })
+    //   }, [])
+    useEffect(() => {
+        axios.get('http://localhost:4001/record').then(resp => {
+            console.log(resp)
+            setState(resp.data)
+            console.log(state)
+        }).catch(err => {
+            console.log(err)
+        })
+    },[])
 
 
-       
-        function validate(e)
-        {
-            e.preventDefault()
-            console.log("email"+email)
-            console.log("uname"+uname)
-            console.log("pass"+pwd)
 
-            state.forEach(el => {
-                if(el.email === email && el.uname === uname) {
-                    if(el.password === pwd) {
-                        alert("log in suceessfull")
-                        navigate('/budget')
-                    } else {
-                        alert("invalid credentials")
-                    }
+    function validate(e) {
+        e.preventDefault()
+        console.log("email" + email)
+        console.log("uname" + uname)
+        console.log("pass" + pwd)
+
+        state.forEach(el => {
+            if (el.email === email && el.uname === uname) {
+                if (el.password === pwd) {
+                    alert("log in suceessfull")
+                    navigate('/budget')
+                } else {
+                    alert("invalid credentials")
                 }
-            })
-            // const temp = state.filter(key =>  key.uname === uname && key.email === email )
-            // console.log(temp)
-            
-            // if(temp.length>0)
-            // {
-            //     console.log("temp"+temp[0])
-            //     if(temp[0].pwd===pwd)
-            //     {
-            //         alert("log in suceessfull")
-            //         console.log(pwd)
+            }
+        })
+        // const temp = state.filter(key =>  key.uname === uname && key.email === email )
+        // console.log(temp)
 
-            //             navigate('/budget')
-            //     }
-            //     else{
-            //          //alert("invalid credentials")
-            //          console.log(pwd)
-            //     }
-            // }
-            
-            // state.map(key =>{
-            //     console.log("here is the exact resposnse"+state.uname)
-            //     if(key.uname===uname)
-            //     {
-            //         if(key.email===email)
-            //         {
-            //             if(key.password===pwd)
-            //             {
-            //                 alert("log in suceessfull")
-            //                 navigate('/budget')
-            //             }
-            //         }
-            //     }
-            //     else
-            //     {
-            //         //alert("invalid credentials")
-            //     }
-            // })
+        // if(temp.length>0)
+        // {
+        //     console.log("temp"+temp[0])
+        //     if(temp[0].pwd===pwd)
+        //     {
+        //         alert("log in suceessfull")
+        //         console.log(pwd)
 
-            //navigate('/budget')
+        //             navigate('/budget')
+        //     }
+        //     else{
+        //          //alert("invalid credentials")
+        //          console.log(pwd)
+        //     }
+        // }
 
-        }
+        // state.map(key =>{
+        //     console.log("here is the exact resposnse"+state.uname)
+        //     if(key.uname===uname)
+        //     {
+        //         if(key.email===email)
+        //         {
+        //             if(key.password===pwd)
+        //             {
+        //                 alert("log in suceessfull")
+        //                 navigate('/budget')
+        //             }
+        //         }
+        //     }
+        //     else
+        //     {
+        //         //alert("invalid credentials")
+        //     }
+        // })
+
+        //navigate('/budget')
+
+    }
 
 
 
@@ -131,9 +143,9 @@ export default function Login() {
                 <Form onSubmit={validate}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>User Name:</Form.Label>
-                        <Form.Control type="text" placeholder="Enter User Name..."  onChange={(e)=>{
+                        <Form.Control type="text" placeholder="Enter User Name..." onChange={(e) => {
                             setUname(e.target.value)
-                            if(uname.length<=6)
+                            if(uname.length<8)
                         {
                             setUerr(false)
                         }
@@ -142,37 +154,38 @@ export default function Login() {
                             setUerr(true)
                         }
 
-                            } }/>
-                    {
-                        uerr ? <Form.Text className="text-muted">
-                            <span style={{color:"red"}}> you cannot use more than 6 letters as a username....!! </span>
-                        </Form.Text>:null
-                    }
+    
+                        }} />
+                        {
+                            uerr ? <Form.Text className="text-muted">
+                                <span style={{ color: "red" }}> you cannot use more than 8 letters as a username....!! </span>
+                            </Form.Text> : null
+                        }
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" onChange={(e)=>{setEmail(e.target.value)
-                         if(!email.includes("@"))
-                         {
-                             setEmailerr(true)
-                         }
-                         else
-                         {
-                             setEmailerr(false)
-                         }
-                        
-                        
-                        }}/>
-                    {
-                       emailerr ? <Form.Text className="text-muted">
-                            <span style={{color:"red"}}> Enter Proper email include @..!! </span>
-                        </Form.Text>:null
-                    }
+                        <Form.Control type="email" placeholder="Enter email" onChange={(e) => {
+                            setEmail(e.target.value)
+                            if (!email.includes("@")) {
+                                setEmailerr(true)
+                            }
+                            else {
+                                setEmailerr(false)
+                            }
+
+
+                        }} />
+                        {
+                            emailerr ? <Form.Text className="text-muted">
+                                <span style={{ color: "red" }}> Enter Proper email include @..!! </span>
+                            </Form.Text> : null
+                        }
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" onChange={(e)=>{
-                            setpwd(e.target.value)}}/>
+                        <Form.Control type="password" placeholder="Password" onChange={(e) => {
+                            setpwd(e.target.value)
+                        }} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
@@ -180,9 +193,27 @@ export default function Login() {
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
+
                     {/* <SocialButton /> */}
 
                 </Form>
+                <div className="App" style={{ marginTop: "30px" }}>
+                    <FacebookLogin
+                        appId="1119805995512513"
+                        autoLoad={true}
+                        fields="name,email,picture"
+                        onClick={componentClicked}
+                        callback={responseFacebook} />
+                </div>
+                <div className="App" style={{ marginTop: "10px" }}>
+                    <GoogleLogin 
+                        clientId="549841564156-qibnmoc418bj0ndrdv4fia6ne7vmbufq.apps.googleusercontent.com"
+                        buttonText="Log In from google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                </div>
             </div>
         </>
     )
